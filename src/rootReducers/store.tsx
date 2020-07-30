6b7fe2552e51ed7e784ec0c0a4aece5bc8c3552a
Luu Hoang Khang <khang.luu@tekmedi.com>
2020-07-30 14:56:28 +0700

    Update store

--- src/rootReducers/store.tsx
+++ src/rootReducers/store.tsx
@@ -14,13 +14,10 @@ const rootReducer = (state = initalState , action : actionType) =>{
     switch (action.type) {
         case "YES":
             return {...state , answer1: action.payload.answer}
-            break;
         case "NO":
             return {...state , answer1: action.payload.answer}
-            break;
         default:
             return state
-            break;
     }
 }
 export const store = createStore(rootReducer)
