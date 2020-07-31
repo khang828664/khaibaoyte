7a4a3b959f3dfd13b3930d4345d4baf51ab677ed
Luu Hoang Khang <khang.luu@tekmedi.com>
2020-07-31 20:35:07 +0700

    notthing

--- src/constant/baseUrl.tsx
+++ src/constant/baseUrl.tsx
@@ -1,2 +1,2 @@
-export const BASE_URL =  "http://14.241.239.78:8095/beta.tek.btc.kiot/"
-export const OLD_URL=  "http://14.241.239.78/tek.btc.kiot/"
+export const BASE_URL = "http://14.241.239.78:8095/beta.tek.btc.kiot/"
+export const OLD_URL =  "http://14.241.239.78/tek.btc.kiot/"
